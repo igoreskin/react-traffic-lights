b74a0b74ef0725640414b909daa9519018bccd9e
Igor Eskin <eskinhd@gmail.com>
2019-07-08 15:05:30 -0400

    Updates

---
+++ precache-manifest.ca3f9d865ef654439cbff288c6ff5ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169137afb63345ce5ead1f5e9dfb8e99",
+    "url": "/react-traffic-lights/index.html"
+  },
+  {
+    "revision": "a66818cdaee2b08617bb",
+    "url": "/react-traffic-lights/static/css/main.8c1b929a.chunk.css"
+  },
+  {
+    "revision": "ebeb9778140b41639dea",
+    "url": "/react-traffic-lights/static/js/2.981886c1.chunk.js"
+  },
+  {
+    "revision": "a66818cdaee2b08617bb",
+    "url": "/react-traffic-lights/static/js/main.e34e1de9.chunk.js"
+  },
+  {
+    "revision": "d6ae2df5d59d04d41040",
+    "url": "/react-traffic-lights/static/js/runtime~main.2aaea9fd.js"
+  },
+  {
+    "revision": "4cbdcd0632b4e0b17f7ae67821a53710",
+    "url": "/react-traffic-lights/static/media/street-view3.4cbdcd06.jpg"
+  }
+]);
